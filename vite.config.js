8d8187ac41c1372ca047a82176bc7eed4bf1d444
alskaaa7 <idwby7@gmail.com>
2025-09-29 19:28:57 +0300

    Fix build with new index.html

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/analytics-dashboard/', // Важно! Название репозитория
+  base: './',
   build: {
     outDir: 'dist'
   }
